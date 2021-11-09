632ae4ad9e5e916f1f6bc6687eb3c5cc74b28059
isakvi <isakvi@kth.se>
2021-11-09 19:19:36 +0100

    #7 feat: Improve UI (#8)
    * Add display of images
    
    * Add improved UI
    
    * Add error handling
    
    Remove unused codelines and imports

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
-from flask import Flask, render_template, jsonify
-from flask_cors import CORS, cross_origin
+from flask import Flask, render_template
 import requests
 
 app = Flask(__name__, static_url_path='',
                   static_folder='react-app/build',
-                  template_folder='react-app/build')               
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+                  template_folder='react-app/build')
 
 
 
 @app.route("/")
-@cross_origin()
 def return_react_app():
     return render_template("index.html")
 
 @app.route("/getMealData")
 def get_meal_data():
     api_url = "https://www.themealdb.com/api/json/v1/1/random.php"
-    response = requests.get(api_url)
-    return response.json()
+    try:
+        response = requests.get(api_url)
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        return "Internal Server Error", 500
--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-
+import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
+  constructor() {
     super();
     this.state = {
-      displayText: "hej",
+      imgSrc: null,
       recipeName: "",
       recipeInstr: "",
     };
     this.getData = this.getData.bind(this);
 }
   render() {
-    return <div>
-
-            <button onClick={this.getData}> Click to be suggested a recipe! </button>
-            <div>{this.state.recipeName}</div>
-            <div>{this.state.recipeInstr}</div>
+    return <div class="grid-container">
+            <div class="item1"> Welcome to the Recipe Suggester!</div>
+            <div class="item5"><button onClick={this.getData}> Click me! </button></div>
+            <div class="item3">{this.state.imgSrc === null ? <div></div> : <img src={this.state.imgSrc} id="pic"></img>}</div>  
+            <div class="item4">
+              <div><h2>{this.state.recipeName}</h2></div>
+              <p>{this.state.recipeInstr}</p></div>
             </div>
   }
 
@@ -25,10 +27,14 @@ class App extends React.Component {
     .then(response => 
     response.json()
     .then(json => {
-      this.setState({recipeName: json.meals[0].strMeal, recipeInstr: json.meals[0].strInstructions})
+      this.setState({
+        imgSrc: json.meals[0].strMealThumb, 
+        recipeName: json.meals[0].strMeal, 
+        recipeInstr: json.meals[0].strInstructions
+      })
     }))
     .catch(err => {
-      throw new Error(err)
+      this.setState({recipeInstr: err})
     })
     
   }
